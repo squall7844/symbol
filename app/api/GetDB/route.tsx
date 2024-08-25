b9dd810fd87e8a7fcb669e6dbb8f03c0896d6e64
squall7844 <mizukitsujimoto09@gmail.com>
2024-08-25 12:03:00 +0900

    もうわからん。。。。

--- app/api/GetDB/route.tsx
+++ app/api/GetDB/route.tsx
@@ -7,18 +7,18 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // メイン関数でDBの接続をする
-export async function GetDB() {
+const connectDB = async () => {
   try {
     await prisma.$connect();
   } catch (err) {
     return Error("DB接続に失敗しました。");
   }
-}
+};
 
 // GETでUSERテーブルの情報をすべて取得
 export const GET = async (req: Request, res: NextResponse) => {
   try {
-    await GetDB();
+    await connectDB();
     const allPosts = await prisma.user.findMany();
     return NextResponse.json({ message: "成功", allPosts }, { status: 200 });
   } catch (err) {
