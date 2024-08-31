9ad905665da88b0a9fb1144761bcf909b0a17fed
squall7844 <mizukitsujimoto09@gmail.com>
2024-08-31 10:35:31 +0900

    testPage修正

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,27 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+const Test = () => {
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
+};
 
-const prisma = new PrismaClient();
-
-async function Seed() {
-  // ユーザーデータを作成
-  const newUser = await prisma.user.create({
-    data: {
-      email: "example5@example.com",
-      password: "password55",
-      name: "test",
-      xym_public_key: "public_key",
-      amount: "100",
-    },
-  });
-  console.log(newUser);
-
-  // データベース接続を切断
-  await prisma.$disconnect();
-}
-
-Seed().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
-
-export default Seed;
+export default Test;
