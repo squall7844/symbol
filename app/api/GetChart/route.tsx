0d2e7c4c141b3c68a5afa87b1d0b3ac8bf53f380
squall7844 <mizukitsujimoto09@gmail.com>
2024-10-21 19:29:10 +0900

    微修正

--- app/api/GetChart/route.tsx
+++ app/api/GetChart/route.tsx
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
+const PAIR = process.env.BITBANK_PAIR || "";
+const URL = process.env.BITBANK_PUBLIC_URL || "";
+
 // ビットバンクのAPIから1週間足のチャートデータを取得
 export const GET = async () => {
   try {
-    const response = await axios.get(
-      `${process.env.BITBANK_PUBLIC_URL}/${process.env.BITBANK_PAIR}/candlestick/1day/2024`
-    );
+    const response = await axios.get(`${URL}/${PAIR}/candlestick/1day/2024`);
     return NextResponse.json(response.data);
   } catch (error) {
     return NextResponse.json(
