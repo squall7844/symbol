f573594656faf0fad38b49b0cd8a8c1a4b34f5f9
squall7844 <mizukitsujimoto09@gmail.com>
2025-02-02 16:03:28 +0900

    振り出しに戻りましょう。。

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,10 +1,11 @@
 import Header from "@/components/Header/Header";
 import { ThemeSwitch } from "@/components/Theme/ThmeSwitch";
 import Price from "@/components/Price/Price";
-import Response from "@/components/Responsive/Response";
+
 import Link from "next/link";
-import SignOut from "@/components/Auth/SignOut";
-import { ROUTES } from "@/components/Route/URL";
+
+import { ROUTES } from "@/components/Utility/URL";
+import Response from "@/components/Utility/Response";
 
 const Home = () => {
   return (
@@ -19,7 +20,6 @@ const Home = () => {
       <Link className="text-center mb-14" href={ROUTES.DETAILS}>
         設定ページ
       </Link>
-      <SignOut />
     </div>
   );
 };
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,28 +1,5 @@
-import { getServerSession } from "next-auth";
+const test = () => {
+  return <div>test</div>;
+};
 
-import SignOut from "@/components/Auth/SignOut";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-
-export default async function TestPage() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return (
-      <div>
-        <h1>Access Denied</h1>
-        <p>
-          You must be logged in to view this page. Please{" "}
-          <a href="/login">log in</a>.
-        </p>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h1>Test Page</h1>
-      <p>Welcome, {session.user?.name}!</p>
-      <SignOut />
-    </div>
-  );
-}
+export default test;
